39671d756e838f73f69b6abfe6af4ef2b3dc17be
Maxim <MaximKomoza@yandex.ru>
2023-03-15 08:11:14 +0700

    disable js hw-5

--- script/homework/hw 5.js
+++ script/homework/hw 5.js
@@ -1,19 +1,19 @@
 // ===== task #1 ===== 
-console.log('===== task #1 =====');
+console.log('\n===== task #1 =====');
 
 let min = (a, b) => a < b ? a : b;
 
 console.log(min(5, 2)); 
 
 // ===== task #2 =====
-console.log('===== task #2 =====');
+console.log('\n===== task #2 =====');
 
 let parity = (num) => num % 2 === 0 ? 'even' : 'odd';
 
 console.log(parity(3));
 
 // ===== task #3 =====
-console.log('===== task #3 =====');
+console.log('\n===== task #3 =====');
 
 // 3.1
 let squareConsole = (num) => console.log(num ** 2);
@@ -24,7 +24,7 @@ let square = (num) => num ** 2;
 console.log(square(4));
 
 // ===== task #4 =====
-console.log('===== task #4 =====');
+console.log('\n===== task #4 =====');
 
 let greeting = (yearsOld) => {
     if (yearsOld >= 0 && yearsOld <= 12) {
@@ -41,7 +41,7 @@ let greeting = (yearsOld) => {
 greeting(26);
 
 // ===== task #5 =====
-console.log('===== task #5 =====');
+console.log('\n===== task #5 =====');
 
 let mult = (a, b) => {
     if (!isNaN(a) && !isNaN(b)) {
@@ -63,7 +63,7 @@ console.log(mult('10', '5'));
 
 
 // ===== task #6 =====
-console.log('===== task #6 =====');
+console.log('\n===== task #6 =====');
 
 let cube = (num) => {
     if (!isNaN (num)) {
@@ -80,7 +80,7 @@ for (let i = 1; i <= 10; i++) {
 cube('i');
 
 // ===== task #7 =====
-console.log('===== task #7 =====');
+console.log('\n===== task #7 =====');
 
 function getArea() {
     return 3.14 * this.radius ** 2;
@@ -92,14 +92,14 @@ function getPerimeter() {
 
 let circle1 = {
     radius: 15,
-    getArea: getArea,
-    getPerimeter: getPerimeter
+    getArea,
+    getPerimeter
 }
 
 let circle2 = {
     radius: 8,
-    getArea: getArea,
-    getPerimeter: getPerimeter
+    getArea,
+    getPerimeter
 }
 
 console.log(`r = ${circle1.radius}, S = ${circle1.getArea()}`);
